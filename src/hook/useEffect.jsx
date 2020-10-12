938a8a6d988c2a223af78b20ac0590cbd1eb1801
John <hzy800@126.com>
2020-10-12 18:14:18 +0800

    Update useEffect.jsx

--- src/hook/useEffect.jsx
+++ src/hook/useEffect.jsx
@@ -6,8 +6,11 @@ function ExampleUseEffect() {
   // 相当于 componenetDidMount 和 componenetDidUpdate
   // 第二个参数(e.g.[xxx])
   // (1) 没有第二个参数：useEffect( () => { 副作用的相关代码... } )
+  //     第二个参数可以省略，这时每次组件渲染时，副效应函数 都会执行。
   // (2) 有第二个参数，但是为空数组：useEffect( () => { 副作用的相关代码... }, [] )
+  //     副效应函数 只会在组件加载进入 DOM 后 只执行一次。
   // (3) 有第二个参数，而且有值：useEffect( () => { 副作用的相关代码... }, [count] )
+  //     指定了第一个参数（副效应函数）的依赖项（e.g. count），只有该变量发生变化时，副效应函数 才会执行。
 
   useEffect(() => {
     // 使用浏览器的API，更新页面标题
